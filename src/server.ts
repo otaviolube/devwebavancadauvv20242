0786dbfadc38d7122b578eca9d6cc001bd387ae4
Otávio Lube dos Santos <otaviolube@gmail.com>
2024-08-22 08:05:38 -0300

    CRUD de usuários finalizado

--- src/server.ts
+++ src/server.ts
@@ -7,15 +7,19 @@ const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
-app.get("/", function (req: Request, res: Response) {
-  res.send("Olá pessoal, tudo bem? Quero fazer mais uma mudança");
-});
-
 //Inserir usuários
 app.post("/user", async function (req: Request, res: Response) {
   //Inserir um usuário ...
   try {
     const userdata = req.body;
+
+    if (!userdata.email) {
+      return res.status(400).json({
+        status: 400,
+        message: "Você precisa passar o email no corpo da requisição",
+      });
+    }
+
     console.log(userdata);
     const newuser = await prisma.user.create({
       data: userdata,
@@ -45,11 +49,56 @@ app.get("/users", async function (req: Request, res: Response) {
 
 // Atualizar usuários
 
-app.put("/user", async function () {});
+app.put("/user/:id", async function (req: Request, res: Response) {
+  try {
+    const id = req.params.id;
+    const body = req.body;
+
+    const updatedUser = await prisma.user.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: body,
+    });
+
+    if (updatedUser) {
+      return res.json({
+        status: 200,
+        updatedUser: updatedUser,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({
+      status: 500,
+      message: error,
+    });
+  }
+});
 
 // Deletar usuários
 
-app.delete("/user", async function () {});
+app.delete("/user/:id", async function (req: Request, res: Response) {
+  try {
+    const id = req.params.id;
+
+    await prisma.user.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    res.status(200).json({
+      status: 200,
+      message: "Usuário deletado com sucesso",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      message: "Fala ao deletar o registro",
+    });
+  }
+});
 
 app.listen(3000, function () {
   console.log("Servidor rodando na porta 3000");
