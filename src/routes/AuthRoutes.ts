07e65eb309f5b6fba4d9b889b0716716f5141779
Otávio Lube dos Santos <otaviolube@gmail.com>
2024-09-12 08:52:13 -0300

    Autenticação

---
+++ src/routes/AuthRoutes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const AuthRouter = Router();
+
+AuthRouter.post("/auth/signin", function () {});
+
+AuthRouter.post("/auth/signup", function () {});
+
+AuthRouter.post("/auth/signout", function () {});
